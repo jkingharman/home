8e3d41f1f98ff28f3f2043ba61fc938f0dcac50e
Jasper King-Harman <j.kingharman@googlemail.com>
2018-06-02 12:37:57 +0100

    Get to article index

--- app/controllers/scraps_controller.rb
+++ app/controllers/scraps_controller.rb
@@ -2,6 +2,7 @@ class ScrapsController < ApplicationController
 
   get "/scraps" do
     @title = "Scraps"
+    @years_published = Scrap.unique_years.sort.reverse
     @scraps = Scrap.order(:created_at)
 
     haml :scraps
--- app/models/scrap.rb
+++ app/models/scrap.rb
@@ -1,3 +1,3 @@
 class Scrap < ActiveRecord::Base
-  
+  scope :unique_years, -> { pluck(:created_at).map(&:year).uniq }
 end
