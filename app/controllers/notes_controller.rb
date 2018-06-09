85caa04ff3e8ce4e3d4eff9afc252244c1ff9ce9
Jasper King-Harman <j.kingharman@googlemail.com>
2018-06-09 17:08:47 +0100

    Add jump links and date list

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,5 +1,12 @@
 class NotesController < ApplicationController
 
+  # candidate for extraction?
+  helpers do
+    def truncate(note_content)
+      note_content[0...180] + "..."
+    end
+  end
+
   get "/notes/\*" do
     slug = request.path_info.gsub("/notes/","")
     @note = Note.build_note(slug)
@@ -8,6 +15,7 @@ class NotesController < ApplicationController
 
   get "/notes" do
     @notes = Note.build_notes
+    @years = @notes.map(&:date).uniq
     haml :notes
   end
 end
