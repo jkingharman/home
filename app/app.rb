2cdaca17c8edfefa380b03ef88cab75592ffe484
Jasper King-Harman <j.kingharman@googlemail.com>
2018-10-30 19:44:56 +0000

    Remove skate code

--- app/app.rb
+++ app/app.rb
@@ -2,12 +2,10 @@ require_relative "controllers/application_controller"
 require_relative "controllers/notes_controller"
 require_relative "controllers/read_controller"
 require_relative "controllers/code_controller"
-require_relative "controllers/skate_controller"
 
 require_relative "helpers/note_helpers"
 
 require_relative "models/note"
 require_relative "models/read"
-require_relative "models/skate"
 
 require_relative "../lib/assets"
--- app/controllers/skate_controller.rb
+++
@@ -1,9 +0,0 @@
-
-class SkateController < ApplicationController
-
-  get "/skate" do
-    @skate = Skate.build_structs
-    binding.pry
-    haml :skate
-  end
-end
