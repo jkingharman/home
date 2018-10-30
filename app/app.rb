440e71cd366e46c5aadb3f05157838bc273490b9
Jasper King-Harman <j.kingharman@googlemail.com>
2018-10-30 19:46:16 +0000

    Remove read code

--- app/app.rb
+++ app/app.rb
@@ -1,11 +1,9 @@
 require_relative "controllers/application_controller"
 require_relative "controllers/notes_controller"
-require_relative "controllers/read_controller"
 require_relative "controllers/code_controller"
 
 require_relative "helpers/note_helpers"
 
 require_relative "models/note"
-require_relative "models/read"
 
 require_relative "../lib/assets"
--- app/models/read.rb
+++
@@ -1,5 +0,0 @@
-class Read < ActiveRecord::Base
-  def self.years
-    all.map { |r| r.created_at.year }.uniq
-  end
-end
