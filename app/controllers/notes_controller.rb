c52f92556c969cc70552c5ca348a2b4250ceb25f
Jasper King-Harman <j.kingharman@googlemail.com>
2018-10-31 10:40:38 +0000

    Sort notes routing

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,12 +1,5 @@
 class NotesController < ApplicationController
 
-  # candidate for extraction?
-  helpers do
-    def truncate(note_content)
-      note_content[0...180] + "..."
-    end
-  end
-
   get "/notes/\*" do
     slug = request.path_info.gsub("/notes/","")
     @note = Note.build_note(slug)
