9c6db586a2a25b25c3517d4788475478a4d1dc30
jkingharman <jkingharman@googlemail.com>
2020-04-23 20:48:03 +0100

    Debug prod

--- tasks/compress_images.rb
+++ tasks/compress_images.rb
@@ -10,23 +10,24 @@ img_dirs = Dir.glob("*").select {|f| File.directory? f }
 puts `pwd`
 
 img_dirs.each do |dir|
-  puts dir
-  Dir.entries(dir).each do |img|
-    puts img
-    ext_name = File.extname(img)
-    base_name = File.basename(img, ".jpg")
-    next if ext_name != ".jpg" || base_name.match?("-compress")
+  puts `ls`
+  # puts dir
+  # Dir.entries(dir).each do |img|
+  #   puts img
+  #   ext_name = File.extname(img)
+  #   base_name = File.basename(img, ".jpg")
+  #   next if ext_name != ".jpg" || base_name.match?("-compress")
+  #
+  #   # Go down to make system call for compressed placeholder
+  #   Dir.chdir(dir)
+  #   puts `pwd`
+  #
+  #   `touch #{base_name}-compress.jpg`
+  #
+  #   in_path = File.new(img)
+  #   out_path = File.new("#{base_name}-compress.jpg")
+  #   Mozjpeg.compress in_path, out_path, arguments: '-quality 70 -quant-table 2 -notrellis'
 
-    # Go down to make system call for compressed placeholder
-    Dir.chdir(dir)
-    puts `pwd`
-
-    `touch #{base_name}-compress.jpg`
-
-    in_path = File.new(img)
-    out_path = File.new("#{base_name}-compress.jpg")
-    Mozjpeg.compress in_path, out_path, arguments: '-quality 70 -quant-table 2 -notrellis'
-
-    Dir.chdir("..")
-  end
+  #   Dir.chdir("..")
+  # end
 end
