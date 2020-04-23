86123a1f2033cff1e98a00a4f596472c334021e8
jkingharman <jkingharman@googlemail.com>
2020-04-23 20:41:53 +0100

    Add asset

--- tasks/compress_images.rb
+++ tasks/compress_images.rb
@@ -15,17 +15,17 @@ img_dirs.each do |dir|
     puts img
     ext_name = File.extname(img)
     base_name = File.basename(img, ".jpg")
-    # next if ext_name != ".jpg" || base_name.match?("-compress")
+    next if ext_name != ".jpg" || base_name.match?("-compress")
 
     # Go down to make system call for compressed placeholder
     Dir.chdir(dir)
     puts `pwd`
 
-    # `touch #{base_name}-compress.jpg`
+    `touch #{base_name}-compress.jpg`
 
-    # in_path = File.new(img)
-    # out_path = File.new("#{base_name}-compress.jpg")
-    # Mozjpeg.compress in_path, out_path, arguments: '-quality 70 -quant-table 2 -notrellis'
+    in_path = File.new(img)
+    out_path = File.new("#{base_name}-compress.jpg")
+    Mozjpeg.compress in_path, out_path, arguments: '-quality 70 -quant-table 2 -notrellis'
 
     Dir.chdir("..")
   end
