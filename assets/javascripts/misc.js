fdf4de6303da79c0bea8976ef32cc4e209ab0ad1
jkingharman <jkingharman@googlemail.com>
2020-07-26 13:53:17 +0100

    Add palm pixel art :)

--- assets/javascripts/misc.js
+++ assets/javascripts/misc.js
@@ -5,9 +5,6 @@ var dark = document.getElementById("darkmode")
 var currentURLForLight = window.location.href.replace(/$|\?mode=light/, "?mode=light")
 var currentURLForDark = window.location.href.replace(/$|\?mode=light/, "")
 
-console.log(light)
-console.log(dark)
-
 if (light) {
   light.setAttribute("href", currentURLForLight)
 } else {
