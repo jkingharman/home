0950d644aed1b90a407cb53b5ddaa254d68db35d
Jasper King-Harman <j.kingharman@googlemail.com>
2021-08-16 16:39:31 +0100

    Blur compressed images

--- tasks/compress_images.rb
+++ tasks/compress_images.rb
@@ -1,4 +1,5 @@
 require "mozjpeg"
+require 'mini_magick'
 
 abort("mozJPEG not supported") unless Mozjpeg.supported?
 
@@ -10,7 +11,8 @@ img_dirs.each do |dir|
   Dir.entries(dir).each do |img|
     ext_name = File.extname(img)
     base_name = File.basename(img, ".jpg")
-    next if ext_name != ".jpg" || base_name.match?("-compress")
+    next if ext_name != ".jpg" || base_name.match?("-compress") || Dir.entries(dir).include?("#{base_name}-compress-blur.jpg")
+
 
     # Go down to make system call for compressed placeholder
     Dir.chdir(dir)
@@ -21,9 +23,11 @@ img_dirs.each do |dir|
     Mozjpeg.compress in_path, out_path, arguments: '-quality 70 -quant-table 2 -notrellis'
 
     # create the blurred version for lazy loading too
-    in_path = File.new(img)
-    out_path = File.new("#{base_name}-compress-blur.jpg")
-    Mozjpeg.compress in_path, out_path, arguments: '-quality 1  -quant-table 2 -notrellis -smooth 100'
+    MiniMagick::Tool::Convert.new do |convert|
+      convert << "#{base_name}-compress.jpg"
+      convert.blur("0x100")
+      convert << "#{base_name}-compress-blur.jpg"
+    end
 
     Dir.chdir("..")
   end
